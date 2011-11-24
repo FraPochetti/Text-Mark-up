c84805eae43cd11fce38e2d53ea76d97eed3f17e
Francesco Pochetti <fra.pochetti@gmail.com>
2011-11-24 09:33:12 +0100

    Function converted into a Class (still work to do in order to bind to GUI)

--- state-parser.py
+++ state-parser.py
@@ -6,22 +6,26 @@
 import sys
 import re
 
-def markup(filename):
-   text = open(filename, 'rU').readlines()
-   html = ''
+class MarkUp:
+ 
+   def __init__(self,filename):
+     self.text = open(filename, 'rU').readlines()
+     self.html = '' 
+
+   def markparser(self):
    
-   status = 'START'
+     status = 'START'
 
-   for line in text:
+     for line in self.text:
 
-    match = re.search(r'\s+\d+\)', line)
+      match = re.search(r'\s+\d+\)', line)
 
-    if status == 'START':
+      if status == 'START':
         if line != '\n':
-          html +=  '<h1>' + line[:-1] + '</h1>\n'
+          self.html +=  '<h1>' + line[:-1] + '</h1>\n'
           status = 'READING'
      
-    elif status == 'READING':
+      elif status == 'READING':
         if line == '\n':
            pass
         elif match:
@@ -34,55 +38,52 @@ def markup(filename):
            paragraph = [line]
            status = 'FIRST_LINE'
      
-    elif status == 'ULIST':
+      elif status == 'ULIST':
         if line.startswith(' -'):
            unordered.append(line)
            status = 'ULIST'
         elif line!= '\n':
            paragraph = [line]
-#           paragraph.append(line)
-           html += '<ul>\n%s</ul>\n' % ''.join('<li>%s</li>\n' % line[2:-1] for line in unordered)
+           self.html += '<ul>\n%s</ul>\n' % ''.join('<li>%s</li>\n' % line[2:-1] for line in unordered)
            status = 'FIRST_LINE'
  
-    elif status == 'OLIST':
+      elif status == 'OLIST':
         if match:
            ordered.append(line)
            status = 'OLIST'
         elif line != '\n':
            paragraph = [line]
-#           paragraph.append(line)
-           html += '<ol>\n%s</ol>\n' % ''.join('<li>%s</li>\n' % line[3:-1] for line in ordered)
+           self.html += '<ol>\n%s</ol>\n' % ''.join('<li>%s</li>\n' % line[3:-1] for line in ordered)
            status = 'FIRST_LINE'
 
-    elif status == 'FIRST_LINE':
+      elif status == 'FIRST_LINE':
         if line == '\n':
-           html += '<h2>%s</h2>\n' % paragraph[0][:-1]
+           self.html += '<h2>%s</h2>\n' % paragraph[0][:-1]
            status = 'READING'
         elif not line.startswith(' -'):
            paragraph.append(line)
            status = 'PARAGRAPH'
     
-    elif status == 'PARAGRAPH':
+      elif status == 'PARAGRAPH':
         if line == '\n':
-           html += '<p>%s</p>\n' % ''.join('%s\n' % line[:-1] for line in paragraph)
+           self.html += '<p>%s</p>\n' % ''.join('%s\n' % line[:-1] for line in paragraph)
            status = 'READING'
         elif not line.startswith(' -'):
            paragraph.append(line)
            status = 'PARAGRAPH'
            
-   html = re.sub(r'(\d+-\d+)', r'<em>\1</em>', html)
-   html = re.sub(r'([\w\-\._]+@[\w\-\._]+)', r'<u>\1</u>', html)
-   html = '<body><html>\n' + html + '</body></html>'
-        
-   print html       
-           
-           
-def main():
-  if len(sys.argv)==2:
-     markup(sys.argv[1])
-  else:
-     print 'No filename specified!'
+     self.html = re.sub(r'(\d+-\d+)', r'<em>\1</em>', self.html)
+     self.html = re.sub(r'([\w\-\._]+@[\w\-\._]+)', r'<u>\1</u>', self.html)
+     self.html = '<body><html>\n' + self.html + '</body></html>'
+
+   def translate(self): 
+     self.markparser()      
+     print self.html       
 
 
 if __name__ == '__main__':
-   main()
+
+   file_name = sys.argv[1]
+   x = MarkUp(file_name)
+   x.translate()
+
